1167e2bdb998a158d96f1e2f12a04304999f1ef6
Arthur Silva <root.arthur@gmail.com>
2020-05-12 12:38:35 -0300

    feat: adjust layout ProductGridComponent

--- src/app/pages/shop/components/product-grid/product-grid.component.ts
+++ src/app/pages/shop/components/product-grid/product-grid.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-product-grid',
-  templateUrl: './product-grid.component.html',
-  styleUrls: ['./product-grid.component.scss']
+  selector: "app-product-grid",
+  templateUrl: "./product-grid.component.html",
+  styleUrls: ["./product-grid.component.scss"],
 })
 export class ProductGridComponent implements OnInit {
+  public products = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
